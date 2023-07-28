204f36d13688f0fb79e434961b582c09a2f19a79
mre7452 <ethan.nybo@gmail.com>
2023-07-27 20:27:50 -0600

    First Netbean Commit.

---
+++ netbeen-projects/first.project.ethan.230727/src/first/project/ethan/pkg230727/FirstProjectEthan230727.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package first.project.ethan.pkg230727;
+
+
+public class FirstProjectEthan230727
+{
+
+   
+    public static void main(String[] args)
+    {
+        int x;
+        x=1;
+        System.out.println("hello world "+x);
+    }
+
+}
